391380f55bf40990785b89a7fc37c86c26a3ae7d
autinerd <sidneyjohn23@icloud.com>
2018-09-18 11:48:12 +0200

    favicon repaired

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,12 +1,14 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
+var hammertime = new Hammer(document.getElementsByTagName("body")[0]);
+hammertime.on("swiperight", function(ev) {
+    $('#sidebar').addClass('active');
+});
+hammertime.on("swipeleft", function(ev) {
+    $('#sidebar').removeClass('active');
+});
+
 $(() => {
-    var hammertime = new Hammer(document.getElementsByTagName("body")[0]);
-    hammertime.on("swiperight", function(ev) {
-        $('#sidebar').addClass('active');
-    });
-    hammertime.on("swipeleft", function(ev) {
-        $('#sidebar').removeClass('active');
-    })
+
 });
