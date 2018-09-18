5ffac55dc3d6203095d54ff9bd45504261a43f2a
autinerd <sidneyjohn23@icloud.com>
2018-09-18 11:55:43 +0200

    next try to get HammerJS to run

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var hammertime = new Hammer(document.getElementsByTagName("body")[0]);
-hammertime.on("swiperight", function(ev) {
-    $('#sidebar').addClass('active');
-});
-hammertime.on("swipeleft", function(ev) {
-    $('#sidebar').removeClass('active');
-});
