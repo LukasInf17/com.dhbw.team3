c2cbec45ef1d553442759b44b7a3d97d0b1e8944
Steffen Walter <serenity@mtmedia.org>
2018-09-28 13:34:07 +0200

    \#13 added gobuffalo mailer package

---
+++ mailers/invit_mail.go
@@ -0,0 +1,21 @@
+package mailers
+
+import (
+	"github.com/gobuffalo/buffalo/mail"
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/pkg/errors"
+)
+
+func SendInvitMail() error {
+	m := mail.NewMessage()
+
+	// fill in with your stuff:
+	m.Subject = "Invit Mail"
+	m.From = "NORELY@invitation-factory.tk"
+	m.To = []string{}
+	err := m.AddBody(r.HTML("invit_mail.html"), render.Data{})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return smtp.Send(m)
+}
