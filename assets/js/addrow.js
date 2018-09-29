af073e68df58ded6c7f918d232bce760fcb94f01
Steffen Walter <serenity@mtmedia.org>
2018-09-29 15:57:05 +0200

    #11 changed third gender to official term "diverse"

--- assets/js/addrow.js
+++ assets/js/addrow.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                 </div>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="gender${counter}" id="inlineRadio${counter}3" value="3" form="invitation-form" checked="checked" />
-                    <label class="form-check-label" for="inlineRadio${counter}3">other</label>
+                    <label class="form-check-label" for="inlineRadio${counter}3">diverse</label>
             </div>
         </td>
         <td data-label="Name: ">
