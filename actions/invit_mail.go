f6ccdc1b8286abe00fa4ed4465f1840dfdfa77b7
Steffen Walter <serenity@mtmedia.org>
2018-10-01 09:48:17 +0200

    #13 added database connection to guests

--- actions/invit_mail.go
+++ actions/invit_mail.go
@@ -4,5 +4,27 @@ import "github.com/gobuffalo/buffalo"
 
 // InvitMailSend default implementation.
 func InvitMailSend(c buffalo.Context) error {
-	return c.Render(200, r.HTML("invit_mail/send.html"))
-}
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	u := c.Value("current_user").(*models.User)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+	// Allocate an empty Invitation
+	invitation := &models.Invitation{}
+
+	// To find the Invitation the parameter invitation_id is used.
+	if err := tx.Find(invitation, c.Param("invitation_id")); err != nil {
+		return c.Error(404, err)
+	}
+
+	if invitation.UserID != u.ID {
+		c.Flash().Add("danger", "You are not allowed to visit this page!")
+		return c.Redirect(302, "/invitations")
+	}
+	guests := &[]models.Guest{}
+	tx.Where("invitationid = ?", invitation.ID).All(guests)
+
+	mailers.SendInvitMail(guests)
+	return c.Redirect(302, "/invitations/"+ c.Param("invitation_id").String())
+	}
--- mailers/invit_mail.go
+++ mailers/invit_mail.go
@@ -6,12 +6,12 @@ import (
 	"github.com/pkg/errors"
 )
 
-func SendInvitMail(guest) error {
+func SendInvitMail() error {
 	m := mail.NewMessage()
 
 	// fill in with your stuff:
 	m.Subject = "Invitation"
-	m.From = "NORELAY@invitation-factory.tk"
+	m.From = "NOREPLAY@invitation-factory.tk"
 	m.To = []string{}
 	err := m.AddBody(r.HTML("invit_mail.html"), render.Data{})
 	if err != nil {
