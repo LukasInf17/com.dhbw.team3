2748bdbbbb3c1bb501a5bb51d98ed388aedb75da
Steffen Walter <serenity@mtmedia.org>
2018-10-15 12:17:48 +0200

    #24 adding postfix, postgresql and pgamdin4 config to git repo

---
+++ pgadmin4/web/config_local.py
@@ -0,0 +1,6 @@
+# config file for pg4admin (web-gui for postgresql database)
+
+LOG_FILE = '/var/log/pgadmin4/1pgadmin4.log'
+SQLITE_PATH = '/var/lib/pgadmin4/1pgadmin4.db'
+SESSION_DB_PATH = '/var/lib/pgadmin4/sessions1'
+STORAGE_DIR = '/var/lib/pgadmin4/storage1'
