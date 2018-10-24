2add3f552737ede31cca4d0d7fa96e437441eb18
Steffen Walter <serenity@mtmedia.org>
2018-10-24 12:21:05 +0200

    #26 implemented script and made it run onece a day

--- delete_unverified_users.sh
+++ delete_unverified_users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-psql -U postgres -d database_name -c "DELETE FROM
+/usr/bin/psql -q -U postgres -d invitation_development -c "DELETE FROM users WHERE created_at < NOW() - INTERVAL '1 days' AND verified = false;"
