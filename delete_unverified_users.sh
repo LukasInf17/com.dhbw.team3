8aff5fc404adc9dd65d360f64bb1b906ef58e899
Steffen Walter <serenity@mtmedia.org>
2018-10-25 11:54:57 +0200

    #26 improved security by not using root to execute the script and by using the .pgpass file for authentication instead of accepting commands without pw auth

--- delete_unverified_users.sh
+++ delete_unverified_users.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-/usr/bin/psql -q -U postgres -d invitation_development -c "DELETE FROM users WHERE created_at < NOW() - INTERVAL '1 days' AND verified = false;"
+/var/lib/postgresql/delete_unverified_users.sh
