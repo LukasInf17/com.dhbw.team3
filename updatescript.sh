3a811ecec29aa9a7831e32132dfa3e81c2aa4e92
Steffen Walter <serenity@mtmedia.org>
2018-10-30 10:49:24 +0100

    #54 showing poster on subdomain an compiling new poster when changes are where made

--- updatescript.sh
+++ updatescript.sh
@@ -8,4 +8,7 @@ reslog=$(git log HEAD..origin/master --oneline)
 if [[ "${reslog}" != "" ]] ; then
 	git merge origin/master
 	buffalo build
+	cd poster
+	pdflatex conference_poster_4.tex
+	cp conference_poster_4.pdf /var/www/html/
 fi
